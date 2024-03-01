7b519fd7db9443db8963ad64d37a56f337095b10
Canuski <ojalexander24@gmail.com>
2024-03-02 00:17:44 +0100

    Add telegram message sender

---
+++ scripts/telegram_sender.py
@@ -0,0 +1,22 @@
+import telegram 
+import telegram.constants 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+async def send_telegram_msg(msg):
+    bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+    
+    # Default formatting
+    formatted_msg = f"<b>Virus Scan Results:</b>\n<b>📄File is empty📄</b>\n\n{msg}\n\n" 
+    if "FOUND" in msg:
+        formatted_msg = f"<b>Virus Scan Results:</b>\n<b>⚠️VIRUS ALERT!⚠️</b>\n\n{msg}\n\n <b>🚨Take imediate action🚨</b>" 
+    elif "OK" in msg:
+        formatted_msg = f"<b>Virus Scan Results:</b>\n<b>✅File is OK!✅</b>\n\n{msg}\n\n" 
+    
+    await bot.sendMessage(chat_id=TELEGRAM_CHAT_ID, text=formatted_msg, parse_mode=telegram.constants.ParseMode.HTML)
